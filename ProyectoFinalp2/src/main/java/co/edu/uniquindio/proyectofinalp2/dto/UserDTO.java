f85c1a2632c7c0dc3d46b579e369aa1cd85c94ba
SebastianEBR <sebastiane.ballesterosr@uqvirtual.edu.co>
2025-09-28 21:35:30 -0500

    Build: se agregan carptas dto/ y service/ ademas la clase UserService

--- ProyectoFinalp2/src/main/java/co/edu/uniquindio/proyectofinalp2/dto/UserDTO.java
+++ ProyectoFinalp2/src/main/java/co/edu/uniquindio/proyectofinalp2/dto/UserDTO.java
@@ -1,4 +1,57 @@
-package co.edu.uniquindio.proyectofinalp2.model;
+package co.edu.uniquindio.proyectofinalp2.dto;
+
+import java.util.List;
 
 public class UserDTO {
+
+    private String idUser;
+    private String fullname;
+    private String email;
+    private String phone;
+    private List<String> addresses;
+
+
+    public UserDTO() {}
+
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<String> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<String> addresses) {
+        this.addresses = addresses;
+    }
+
 }
