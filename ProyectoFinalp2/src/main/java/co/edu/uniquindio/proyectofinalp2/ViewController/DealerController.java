974eb3f3840305b14bd3016a924f35c358aeed1a
Alejandrfrancocardenas <alejandro.francoc1@uqvirtual.edu.co>
2025-11-09 21:56:05 -0500

    Actualiza DealerController y DealerView

--- ProyectoFinalp2/src/main/java/co/edu/uniquindio/proyectofinalp2/ViewController/DealerController.java
+++ ProyectoFinalp2/src/main/java/co/edu/uniquindio/proyectofinalp2/ViewController/DealerController.java
@@ -1,6 +1,13 @@
 package co.edu.uniquindio.proyectofinalp2.ViewController;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class DealerController {
+@FXML
+private Button btnEntregasRealizadas;
+@FXML
+    private void onEntregasRealizadas(){
 
-
+}
 }
